cf0ce7927ffc5997a7149e49f2d822d1df331370
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-04-08 22:19:18 +0900

    [Gold III] Title: 다리 만들기, Time: 1916 ms, Memory: 34164 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,66 +1,63 @@
 from collections import deque
+import sys
+input=sys.stdin.readline
 
-# 입력값
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-visited = [[0]*n for _ in range(n)]
+visited = [[0]*N for _ in range(N)]
 num = 1
 res = int(1e9)
 
-# 1번째 bfs (섬 구분)
 def bfs(i,j):
+  dx=[1,0,-1,0]
+  dy=[0,-1,0,1]
   que = deque()
   que.append([i,j])
   while que:
     x, y = que.popleft()
-    for (h, w) in [[1,0],[0,1],[-1,0],[0,-1]]:
-      dx, dy = x+h, y+w
-      if 0<=dx<n and 0<=dy<n and not visited[dx][dy] and graph[dx][dy]:
-        visited[dx][dy] = 1
-        graph[dx][dy] = num
-        que.append([dx,dy])
+    for k in range(4):
+      nx=x+dx[k]
+      ny=y+dy[k]
+      if 0<=nx<N and 0<=ny<N and not visited[nx][ny] and graph[nx][ny]:
+        visited[nx][ny] = 1
+        graph[nx][ny] = num
+        que.append([nx,ny])
 
-        
-# 2번째 bfs (최단거리 구하기)
-def bfs2(v):
+def bfs2(index):
+  dx=[1,0,-1,0]
+  dy=[0,-1,0,1]
   queue = deque()
-  dist = [[-1]*n for _ in range(n)]
+  dist = [[-1]*N for _ in range(N)]
 
-  for i in range(n):
-    for j in range(n):
-      if graph[i][j]==v:
+  for i in range(N):
+    for j in range(N):
+      if graph[i][j]==index:
         dist[i][j] = 0
         queue.append([i,j])
 
   while queue:
     x, y = queue.popleft()
-    for (w, h) in [[1,0],[0,1],[-1,0],[0,-1]]:
-      dx, dy = x+w, y+h
-      if 0<=dx<n and 0<=dy<n:
-        # 다른 섬 만났다! 연결됨!
-        if graph[dx][dy] and graph[dx][dy]!=v:
+    for k in range(4):
+      nx=x+dx[k]
+      ny=y+dy[k]
+      if 0<=nx<N and 0<=ny<N:
+        if graph[nx][ny] and graph[nx][ny]!=index:
           return dist[x][y]
-        # 물이고 아직 다리 없는 곳
-        elif (not graph[dx][dy]) and dist[dx][dy]==-1:
-          dist[dx][dy] = dist[x][y]+1
-          queue.append([dx,dy])
+        elif (not graph[nx][ny]) and dist[nx][ny]==-1:
+          dist[nx][ny] = dist[x][y]+1
+          queue.append([nx,ny])
 
   return int(1e9)
 
-
-
-# 섬 구분
-for i in range(n):
-  for j in range(n):
+for i in range(N):
+  for j in range(N):
     if graph[i][j] and not visited[i][j]:
       visited[i][j] = 1
       graph[i][j] = num
       bfs(i,j)
       num += 1
 
-
-# 최단거리 구하기
-for v in range(1,num):
-  res = min(res, bfs2(v))
+for index in range(1,num):
+  res = min(res, bfs2(index))
 print(res)
