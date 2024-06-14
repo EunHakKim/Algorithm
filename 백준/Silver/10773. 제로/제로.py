8356e658725ee186be75e1ab4fac884fb3c1a1a1
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-06-14 23:52:34 +0900

    [Silver IV] Title: 제로, Time: 100 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+k=int(input())
+nums=deque()
+for _ in range(k):
+    n=int(input())
+    if n==0:
+        nums.pop()
+    else:
+        nums.append(n)
+
+print(sum(nums))
