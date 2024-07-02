fda81bf5243f0cb93b67110ea00491f137b29673
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-07-02 23:56:15 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 160 ms, Memory: 33972 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,18 +1,19 @@
 import sys
 import heapq
+input = sys.stdin.readline
 
 n = int(input())
 cards = []
 for i in range(n):
-    heapq.heappush(cards, int(sys.stdin.readline()))
+    heapq.heappush(cards, int(input()))
 
-total_cost = 0
+total = 0
 while len(cards) > 1:
     a = heapq.heappop(cards)
     b = heapq.heappop(cards)
-    sum_value = a + b
+    sum = a + b
 
-    total_cost += sum_value
-    heapq.heappush(cards, sum_value)
+    total += sum
+    heapq.heappush(cards, sum)
 
-print(total_cost)
+print(total)
