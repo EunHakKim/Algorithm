5187aa92f2e724f3d88bb50b9afcda3cc95b3062
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-07-10 00:17:39 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,44 +1,59 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-n,m = map(int,input().split())
-graph = []
-visited = [[0] * m for _ in range(n)]
+n, m = map(int,input().split())
 r,c,d = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+dx=[1,0,-1,0]
+dy=[0,1,0,-1]
+cnt=0
 
-# d => 0,3,2,1 순서로 돌아야한다.
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
-
-# 처음 시작하는 곳 방문 처리
-visited[r][c] = 1
-cnt = 1
-
-while 1:
-    flag = 0
-    # 4방향 확인
-    for _ in range(4):
-        # 0,3,2,1 순서 만들어주기위한 식
-        nx = r + dx[(d+3)%4]
-        ny = c + dy[(d+3)%4]
-        # 한번 돌았으면 그 방향으로 작업시작
-        d = (d+3)%4
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-            if visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                cnt += 1
-                r = nx
-                c = ny
-                #청소 한 방향이라도 했으면 다음으로 넘어감
-                flag = 1
+while True:
+    if graph[r][c]==0 and not visited[r][c]:
+        visited[r][c]=True
+        cnt+=1
+        continue
+    clean=False
+    for i in range(4):
+        nx = c + dx[i]
+        ny = r + dy[i]
+        if graph[ny][nx]==0 and not visited[ny][nx]:
+            clean=True
+    if not clean:
+        if d==0:
+            if graph[r+1][c]==1:
                 break
-    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
-        if graph[r-dx[d]][c-dy[d]] == 1: #후진했는데 벽
-            print(cnt)
-            break
+            else:
+                r+=1
+        elif d==1:
+            if graph[r][c-1]==1:
+                break
+            else:
+                c-=1
+        elif d==2:
+            if graph[r-1][c]==1:
+                break
+            else:
+                r-=1
         else:
-            r,c = r-dx[d],c-dy[d]
+            if graph[r][c+1]==1:
+                break
+            else:
+                c+=1
+    else:
+        d=(d+4-1)%4
+        if d==0:
+            if graph[r-1][c]==0 and not visited[r-1][c]:
+                r-=1
+        elif d==1:
+            if graph[r][c+1]==0 and not visited[r][c+1]:
+                c+=1
+        elif d==2:
+            if graph[r+1][c]==0 and not visited[r+1][c]:
+                r+=1
+        else:
+            if graph[r][c-1]==0 and not visited[r][c-1]:
+                c-=1
+
+print(cnt)
