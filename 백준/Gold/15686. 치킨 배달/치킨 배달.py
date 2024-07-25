1a53a602938294c3f1b3c0ac060040b410596621
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-07-25 17:10:56 +0900

    [Gold V] Title: 치킨 배달, Time: 444 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,51 +1,39 @@
-N,M = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-min_ans = 99999999
+n, m = map(int,input().split())
+house = []
+chicken = []
+for i in range(n):
+    temp = list(map(int,input().split()))
+    for j in range(n):
+        if temp[j] == 1:
+            house.append((i,j))
+        elif temp[j]==2:
+            chicken.append((i,j))
 
-home = []
+visited = [False]*len(chicken)
+minimun = 10**9 
 
-chick = []
-
-for i in range(N): # 집과 치킨의 좌표를 리스트에 넣어준다.
-    row = list(map(int,input().split()))
-
-    for j in range(N):
-        if row[j] == 1:
-            home.append((i,j))
-        elif row[j]==2:
-            chick.append((i,j))
-
-visited = [False] * len(chick)
-
-def dfs(idx,cnt):
-
-    global min_ans
-
-    if cnt == M:
-        # print(visited[:])
-
-        ans = 0
-
-        for i in home: # 집 좌표에 대해 모든 치킨집과의 거리를 구함
-            distance = 9999999 # 거리를 큰 수로 정의
+def back(index,cnt):
+    global minimun
+    if cnt==m:
+        clen = 0
+        for i in house:
+            cmin = 10**9 
             for j in range(len(visited)):
                 if visited[j]:
-                    check_num = abs(i[0]-chick[j][0])+abs(i[1]-chick[j][1])
-                    distance = min(distance,check_num) # 각 집에 대해 치킨 거리가 최소인 값을 구함
-            ans +=distance
-        min_ans = min(ans,min_ans)
-
+                    distance = abs(i[0]-chicken[j][0])+abs(i[1]-chicken[j][1])
+                    cmin = min(cmin, distance)
+            clen += cmin
+        minimun = min(minimun, clen)
         return
 
-    for i in range(idx,len(chick)):
+    for i in range(index, len(chicken)):
         if not visited[i]:
-
             visited[i] = True
-            dfs(i+1,cnt+1)
+            back(i+1,cnt+1)
             visited[i]=False
 
-
-
-dfs(0,0)
-
-print(min_ans)
+back(0,0)
+print(minimun)
