7793ff17e2a83fb5dbd5f1a951a62b5b852dc864
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-11-11 00:58:38 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -0,0 +1,40 @@
+from copy import copy
+import sys
+sys.setrecursionlimit(10**6)
+def solution(n, m, x, y, r, c, k):
+    answer = ''
+    if k < abs(r - x) + abs(c - y):
+        return 'impossible'
+    if (k - abs(r - x) + abs(c - y)) % 2 == 1:
+        return 'impossible'
+    
+    while k - (abs(r - x) + abs(c - y)) > 0 and x < n:
+        answer += 'd'
+        x += 1
+        k -= 1
+        
+    while k - (abs(r - x) + abs(c - y)) > 0 and y > 1:
+        answer += 'l'
+        y -= 1
+        k -= 1
+    
+    remain = k - (abs(r - x) + abs(c - y))
+    for i in range(remain//2):
+        answer += 'rl'
+        k -= 2
+    
+    row = abs(x - r)
+    col = abs(y - c)
+    if x < r:
+        answer += 'd' * row
+
+    if y > c:
+        answer += 'l' * col
+
+    if y < c:
+        answer += 'r' * col
+
+    if x > r:
+        answer += 'u' * row
+    
+    return answer
