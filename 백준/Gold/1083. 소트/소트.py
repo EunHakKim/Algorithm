f5cee327d52aff1a24fc5ac925e9ca24fe0189d4
EunHakKim <152606597+EunHakKim@users.noreply.github.com>
2024-11-16 19:59:52 +0900

    [Gold IV] Title: 소트, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1083. 소트/소트.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+s = int(input())
+target = 0
+
+while s > 0 and target < n:
+    if a[target] == max(a[target:min(target+s+1, n)]):
+        target += 1
+        continue
+
+    idx = a.index(max(a[target:min(target+s+1, n)]))
+    temp = a[idx]
+    s -= idx - target
+    a.remove(temp)
+    a.insert(target, temp)
+    target += 1
+
+print(" ".join(map(str, a)))
