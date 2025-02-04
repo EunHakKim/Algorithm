f23b7d463a23dd2525b3ddedd0112aede6388dd6
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-02-05 02:05:23 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -7,20 +7,15 @@ for _ in range(n):
     a.append(int(input()))
 
 a.sort()
-left, right = 0, 1
+left, right = 0, 0
 diff = a[-1] - a[0]
 
 while right < n:
     if m > a[right]-a[left]:
         right += 1
     elif m < a[right]-a[left]:
-        if left + 1 >= right:
-            diff = min(diff, a[right]-a[left])
-            left += 1
-            right += 1 
-        else:
-            diff = min(diff, a[right]-a[left])
-            left += 1
+        diff = min(diff, a[right]-a[left])
+        left += 1
     else:
         diff = m
         break
