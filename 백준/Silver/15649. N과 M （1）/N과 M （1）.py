5da70da5c8893ed46c661d9c631e91c0ccdddbe7
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-03-19 03:57:30 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,18 @@
-N, M = map(int, input().split())
-result=[str(x) for x in range(1,N+1)]
-new_result=[]
+import sys
+input = sys.stdin.readline
 
-while(M>1):
-    for i in range(len(result)):
-        for j in range(1,N+1):
-            if str(j) not in result[i]:
-                new_result.append(result[i]+str(j))
-    result=new_result
-    new_result=[]
-    M-=1
+n, m = map(int, input().split())
+ans = []
 
-for i in range(len(result)):
-    for j in range(len(result[0])):
-        print(result[i][j], end=" ")
-    print()
+def back(depth):
+    if depth == m:
+        print(*ans)
+        return
+    
+    for i in range(1, n + 1):
+        if i not in ans:
+            ans.append(i)
+            back(depth + 1)
+            ans.pop()
+
+back(0)
