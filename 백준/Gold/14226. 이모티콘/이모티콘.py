8c77fa5eb7ad7dbc9ae2a86183786f7c9a09316b
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-03-18 23:13:57 +0900

    [Gold IV] Title: 이모티콘, Time: 128 ms, Memory: 63840 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,28 +1,25 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-S=int(input())
-
-dist = [[-1]* (S+1) for _ in range(S+1)]
-queue = deque()
-queue.append((1,0))
-dist[1][0] = 0
-
+target = int(input())
+visited = [[-1] * 2000 for _ in range(2000)]
+queue = deque([(1, 0)])
+visited[1][0] = 0
 while queue:
-    s,c = queue.popleft()
-    if dist[s][s] == -1:
-        dist[s][s] = dist[s][c] + 1
-        queue.append((s,s))
-    if s+c <= S and dist[s+c][c] == -1:
-        dist[s+c][c] = dist[s][c] + 1
-        queue.append((s+c, c))
-    if s-1 >= 0 and dist[s-1][c] == -1:
-        dist[s-1][c] = dist[s][c] + 1
-        queue.append((s-1, c))
-
-answer = 1001
-
-for i in range(S+1):
-    if dist[S][i] != -1:
-        if answer > dist[S][i]:
-            answer = dist[S][i]
-print(answer)
+    s, c = queue.popleft()
+    if target == s:
+        print(visited[s][c])
+        break
+    #1
+    if visited[s][s] == -1:
+       visited[s][s] = visited[s][c] + 1
+       queue.append((s, s))
+    #2
+    if s + c < 2000 and visited[s + c][c] == -1:
+        visited[s + c][c] = visited[s][c] + 1
+        queue.append((s + c, c))
+    #3
+    if s - 1 >= 0 and visited[s - 1][c] == -1:
+        visited[s - 1][c] = visited[s][c] + 1
+        queue.append((s - 1, c))
