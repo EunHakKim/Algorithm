76f5b37df20bfc7b43aa38228e216ccec6340245
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-03-21 01:52:20 +0900

    [Silver II] Title: 최대 힙, Time: 112 ms, Memory: 40388 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+hq = []
+for _ in range(n):
+    num = int(input())
+    if num == 0:
+        if len(hq) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(hq) * -1)
+    else:
+        heapq.heappush(hq, num * -1)
