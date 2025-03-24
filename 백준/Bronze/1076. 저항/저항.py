617e36f3355878d3768f614f28142747102323fa
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-03-24 22:08:55 +0900

    [Bronze II] Title: 저항, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,15 @@
+color = {
+    'black': ['0', 1],
+    'brown': ['1', 10],
+    'red': ['2', 100],
+    'orange': ['3', 1000],
+    'yellow': ['4', 10000],
+    'green': ['5', 100000],
+    'blue': ['6', 1000000],
+    'violet': ['7', 10000000],
+    'grey': ['8', 100000000],
+    'white': ['9', 1000000000]
+}
+
+arr = [input().strip() for _ in range(3)]
+print(int(color[arr[0]][0] + color[arr[1]][0]) * color[arr[2]][1])
