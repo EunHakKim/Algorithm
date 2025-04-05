398c6518f55363d66747301e64adc3b821178b46
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-04-05 22:08:24 +0900

    [Silver I] Title: 귀여운 라이언, Time: 444 ms, Memory: 51924 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dolls = list(map(int, input().split()))
+
+# 1이 3개미만이면 -1 출력
+if dolls.count(1) < k:
+    print(-1)
+    exit()
+
+lion_cnt, left = 1, 0
+while dolls[left] != 1:
+    left += 1
+
+right = left
+while lion_cnt < k:
+    right += 1
+    if dolls[right] == 1:
+        lion_cnt += 1
+
+ans = right - left + 1
+while right < n:
+    right += 1
+    if right == n:
+        break
+    if dolls[right] == 1:
+        left += 1
+        while dolls[left] != 1:
+            left += 1
+    ans = min(ans, right - left + 1)
+
+print(ans)
