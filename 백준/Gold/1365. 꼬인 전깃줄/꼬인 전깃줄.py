3de0a3ccead8828290e128d15cfde15e99ae44f0
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-04-26 21:03:15 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 144 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -5,14 +5,14 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
+    st, en = 0, len(arr) - 1
+    while st <= en:
+        mid = (st + en) // 2
         if arr[mid] < target:
-            left = mid + 1
+            st = mid + 1
         else:
-            right = mid - 1
-    return left
+            en = mid - 1
+    return st
 
 lis = [nums[0]]
 for i in range(1, n):
