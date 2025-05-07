9bfdaf0eff124fd0061ed42e7320d1e732a21f0f
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-05-07 20:59:00 +0900

    [Gold IV] Title: 세 수의 합, Time: 268 ms, Memory: 67232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,21 +1,16 @@
 import sys
 input = sys.stdin.readline
-
 n = int(input())
-u = []
-for _ in range(n):
-    u.append(int(input()))
+u = [int(input()) for _ in range(n)]
 u.sort()
 
-for i in range(n - 1, -1, -1):
-    for j in range(i + 1):
-        st, en = j, i
-        while st <= en:
-            temp = u[st] + u[j] + u[en]
-            if temp == u[i]:
-                print(temp)
-                exit()
-            if temp > u[i]:
-                en -= 1
-            else:
-                st += 1
+u_sum = set()
+for x in u:
+    for y in u:
+        u_sum.add(x+y)
+
+for i in range(n-1, -1, -1):
+    for j in range(i+1):
+        if u[i]-u[j] in u_sum:
+            print(u[i])
+            exit()
