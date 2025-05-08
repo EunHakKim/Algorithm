9324087ae34f5ceebba914f188e38214a4dcc3b0
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-05-08 20:52:21 +0900

    [Gold IV] Title: 이모티콘, Time: 80 ms, Memory: 40444 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -2,24 +2,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-target = int(input())
-visited = [[-1] * 2000 for _ in range(2000)]
-queue = deque([(1, 0)])
+s = int(input())
+visited = [[-1] * 1001 for _ in range(1001)]
 visited[1][0] = 0
+queue = deque([(1, 0)])
 while queue:
-    s, c = queue.popleft()
-    if target == s:
-        print(visited[s][c])
-        break
-    #1
-    if visited[s][s] == -1:
-       visited[s][s] = visited[s][c] + 1
-       queue.append((s, s))
-    #2
-    if s + c < 2000 and visited[s + c][c] == -1:
-        visited[s + c][c] = visited[s][c] + 1
-        queue.append((s + c, c))
-    #3
-    if s - 1 >= 0 and visited[s - 1][c] == -1:
-        visited[s - 1][c] = visited[s][c] + 1
-        queue.append((s - 1, c))
+    b, c = queue.popleft()
+    if b == s:
+        print(visited[b][c])
+        exit()
+
+    # 1
+    if visited[b][b] == -1:
+        visited[b][b] = visited[b][c] + 1
+        queue.append((b, b))
+
+    # 2
+    if b + c < 1001:
+        if visited[b + c][c] == -1:
+            visited[b + c][c] = visited[b][c] + 1
+            queue.append((b + c, c))
+
+    # 3
+    if b - 1 >= 0:
+        if visited[b - 1][c] == -1:
+            visited[b - 1][c] = visited[b][c] + 1
+            queue.append((b - 1, c))
