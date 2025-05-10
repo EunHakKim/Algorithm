95f5fb0faa1853a3427bb35349365d55cdd972f7
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-05-10 23:30:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,60 +1,66 @@
-import java.util.ArrayList;
-import java.util.Collections;
+
+import java.awt.*;
+import java.util.*;
+import java.io.*;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-
-    public static int N;
+    public static int n;
     public static int[][] graph;
     public static boolean[][] visited;
-    public static int cnt;
-    public static List<Integer> results;
-    public static int[] dx = {1,0,-1,0};
-    public static int[] dy = {0,-1,0,1};
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        graph = new int[26][26];
-        visited = new boolean[26][26];
-        results = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        graph = new int[n][n];
+        visited = new boolean[n][n];
 
-        for(int i=0;i<N;i++){
-            String line = sc.next();
-            for (int j=0;j<N;j++){
-                graph[i][j]=line.charAt(j) - '0';
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = s.charAt(j) - '0';
             }
         }
 
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(graph[i][j]==1 && !visited[i][j]){
-                    cnt=0;
-                    visited[i][j]=true;
-                    dfs(j,i);
-                    results.add(cnt);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == 1 && !visited[i][j]) {
+                    int temp = bfs(j, i);
+                    result.add(temp);
                 }
             }
         }
-        Collections.sort(results);
-        System.out.println(results.size());
-        for (Integer result : results) {
-            System.out.println(result);
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
         }
     }
 
-    public static void dfs(int x, int y){
-        cnt+=1;
-        for(int k=0;k<4;k++){
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            if(0<=nx && nx<N && 0<=ny && ny<N){
-                if (graph[ny][nx]==1 && !visited[ny][nx]){
-                    visited[ny][nx]=true;
-                    dfs(nx,ny);
+    public static int bfs(int x, int y) {
+        visited[y][x] = true;
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x, y));
+        int cnt = 1;
+
+        while (!queue.isEmpty()) {
+            Point cp = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = cp.x + dx[k];
+                int ny = cp.y + dy[k];
+                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                    if (graph[ny][nx] == 1 && !visited[ny][nx]) {
+                        visited[ny][nx] = true;
+                        cnt ++;
+                        queue.offer(new Point(nx, ny));
+                    }
                 }
             }
         }
+
+        return cnt;
     }
 }
