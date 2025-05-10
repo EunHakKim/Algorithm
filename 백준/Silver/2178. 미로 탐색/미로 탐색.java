092f931344a708fbd62d0579bb31d0d31cd3f53a
김은학 <152606597+EunHakKim@users.noreply.github.com>
2025-05-10 22:37:17 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,48 @@
 import java.awt.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static int N;
-    public static int M;
-    public static int[][] graph;
-    public static boolean[][] visited;
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
+    public static int n, m;
+    public static int[][] miro;
+    public static int[][] visited;
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
     public static Queue<Point> queue;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        graph = new int[101][101];
-        visited = new boolean[101][101];
+        miro = new int[n][m];
+        visited = new int[n][m];
 
-        for(int i=0;i<N;i++){
-            String line = sc.next();
-            for(int j = 0; j<M;j++){
-                graph[i][j] = line.charAt(j) - '0';
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                miro[i][j] = s.charAt(j) - '0';
+                visited[i][j] = -1;
             }
         }
-        bfs(0,0);
-    }
 
-    public static void bfs(int x, int y) {
+        visited[0][0] = 1;
         queue = new LinkedList<>();
-
-        queue.offer(new Point(x, y));
-        while (!queue.isEmpty()){
-            Point temp = queue.poll();
-
-            for(int k=0;k<4;k++){
-                int nx = temp.x + dx[k];
-                int ny = temp.y + dy[k];
-
-                if(0<=nx && nx<M && 0<=ny && ny<N){
-                    if(graph[ny][nx]==1 && !visited[ny][nx]){
+        queue.offer(new Point(0, 0));
+        while (!queue.isEmpty()) {
+            Point cp = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = cp.x + dx[k];
+                int ny = cp.y + dy[k];
+                if (0 <= nx && nx < m && 0 <= ny && ny < n) {
+                    if (miro[ny][nx] == 1 && visited[ny][nx] == -1) {
+                        visited[ny][nx] = visited[cp.y][cp.x] + 1;
                         queue.offer(new Point(nx, ny));
-                        visited[ny][nx]=true;
-                        graph[ny][nx]=graph[temp.y][temp.x]+1;
                     }
                 }
             }
         }
-        System.out.println(graph[N-1][M-1]);
+        System.out.println(visited[n - 1][m - 1]);
     }
 }
